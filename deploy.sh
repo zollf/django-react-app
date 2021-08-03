a445ac20a43d0f4508adea91e2ef5db8eddf0924
Dylan Kennedy <dylan@theleagueagency.com.au>
2021-08-03 22:30:29 +0800

    Fixed ECR Delete Image

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ docker login -u AWS -p $(aws ecr get-login-password --region ap-southeast-2) 703
 docker push 703161335764.dkr.ecr.ap-southeast-2.amazonaws.com/dylan_test_app:latest
 aws ecs update-service --cluster dylan_test_app --service deploy_service --force-new-deployment > /dev/null 
 
-IMAGES_TO_DELETE=$(aws ecr list-images --region ap-southeast-2 --repository-name dylan-test-app --filter tagStatus=UNTAGGED --query imageIds[*] --output json)
-aws ecr batch-delete-image --region ap-southeast-2 --repository-name dylan-test-app --image-ids "$IMAGES_TO_DELETE" || true
+IMAGES_TO_DELETE=$(aws ecr list-images --region ap-southeast-2 --repository-name dylan_test_app --filter tagStatus=UNTAGGED --query imageIds[*] --output json)
+aws ecr batch-delete-image --region ap-southeast-2 --repository-name dylan_test_app --image-ids "$IMAGES_TO_DELETE" || true
