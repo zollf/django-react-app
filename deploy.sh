24213e31a65084cdc811ff95e88f34bae8ec6a0d
Dylan Kennedy <dylan@theleagueagency.com.au>
2021-08-03 22:26:05 +0800

    Fixed Deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ docker build -f Dockerfile -t dylan_test_app .
 docker tag dylan_test_app:latest 703161335764.dkr.ecr.ap-southeast-2.amazonaws.com/dylan_test_app:latest
 docker login -u AWS -p $(aws ecr get-login-password --region ap-southeast-2) 703161335764.dkr.ecr.ap-southeast-2.amazonaws.com
 docker push 703161335764.dkr.ecr.ap-southeast-2.amazonaws.com/dylan_test_app:latest
-aws ecs update-service --cluster dylan_test_app --service deploy_service --force-new-deployment  
+aws ecs update-service --cluster dylan_test_app --service deploy_service --force-new-deployment > /dev/null 
+
+IMAGES_TO_DELETE=$(aws ecr list-images --region ap-southeast-2 --repository-name dylan-test-app --filter tagStatus=UNTAGGED --query imageIds[*] --output json)
+aws ecr batch-delete-image --region ap-southeast-2 --repository-name dylan-test-app --image-ids "$IMAGES_TO_DELETE" || true
