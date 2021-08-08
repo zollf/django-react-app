19eb2d1a8388d33b75beb2c47faaccb0eb8f27ce
= <22709469@student.uwa.edu.au>
2021-08-08 15:41:09 +0800

    Added Migrations

--- app/test_app/__init__.py
+++ app/test_app/__init__.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 class TestAppConfig(AppConfig):
   name = 'app.test_app'
+  label = 'test_app'
   verbose_name = 'Test App'
 
 default_app_config = 'app.test_app.TestAppConfig'
--- app/test_app/models.py
+++ app/test_app/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-class TestUsers(models.Model):
+class Users(models.Model):
   name = models.CharField(db_index=True, max_length=255, unique=True)
